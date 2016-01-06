cbfffb5ea5bc755ecd07eded6cd62d19a1f757e9
oilandsugar <tommy_pm@hotmail.com>
2016-01-06 11:51:05 -0500

    start building single stories template

--- cimmerian-app/services.js
+++ cimmerian-app/services.js
@@ -8,11 +8,47 @@
 
         vm.stories = [
           {
-            name: 'projet 1',
-            date: '2015 01 02',
-            duration: "25 minutes",
+            name: 'story no.1',
+            date: '2015-01-02',
+            duration: "25:00",
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In gravida libero turpis, nec mattis odio imperdiet at. Maecenas venenatis tincidunt lacinia. Phasellus finibus augue metus, vel lobortis dui rutrum vitae.',
-            type: 'video'
+            intention: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In gravida libero turpis, nec mattis odio imperdiet at. Maecenas venenatis tincidunt lacinia. Phasellus finibus augue metus, vel lobortis dui rutrum vitae.',
+            type: 'video',
+            video: '/urltovideo',
+            photos: [
+              {
+                id: 1
+                url: '/urltofile',
+                caption: 'caption'
+              },
+              {
+                id: 2
+                url: '/urltofile',
+                caption: 'caption'
+              },
+              {
+                id: 3
+                url: '/urltofile',
+                caption: 'caption'
+              }
+            ],
+            btsPhotos: [
+              {
+                id: 1
+                url: '/urltofile',
+                caption: 'caption'
+              },
+              {
+                id: 2
+                url: '/urltofile',
+                caption: 'caption'
+              },
+              {
+                id: 3
+                url: '/urltofile',
+                caption: 'caption'
+              }
+            ]
           }
         ];
 
