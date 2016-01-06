172f70bed89dece518e67338aec30f8a2f907011
oilandsugar <tommy_pm@hotmail.com>
2016-01-06 11:38:57 -0500

    debug single story route

--- cimmerian-app/controllers/main-controller.js
+++ cimmerian-app/controllers/main-controller.js
@@ -5,13 +5,15 @@
         .module('cimmerian-app')
         .controller("MainController", ['$rootScope', mainCtrlFunc]);
 
-    function mainCtrlFunc($rootScope){
+    function mainCtrlFunc($rootScope, $stateParams){
       var vm = this;
       vm.title = 'Cimmerian';
 
       $rootScope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
         $rootScope.bodyClass = toState.name;
       });
+
+      console.log($stateParams.storyId);
 
       vm.getClass = function() {
         return $rootScope.bodyClass;
